8b405e0e3667335a04bba756b0002c39001058e7
Samuli Lehtonen <samulilehtonen33@gmail.com>
2021-01-23 23:34:45 +0200

    slightly faster listing version

--- listingapp/src/App.js
+++ listingapp/src/App.js
@@ -13,54 +13,68 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      currCategory: "gloves",
+      currCategory: null,
       beanies: null,
       facemasks: null,
-      gloves: null,
+      gloves: null
       
     }
     this.stockRegex = /<INSTOCKVALUE>(.*)<\/INSTOCKVALUE>/
     this.manufacturerAV = []
     
+    
   }
 
-// fetchAvailability = async (manufacturer) =>{
-//   let config = {
-//     headers: {
-//       'Access-Control-Allow-Origin': "*"
-//     }
-//   }
+getCategory = () =>{
+  switch(this.state.currCategory){
+    case "beanies":
+      return this.state.beanies;
+    case "facemasks":
+      return this.state.facemasks
+    case "gloves":
+      return this.state.gloves
+    default:
+      return <p>No current caregory selected. Try pressing the button or reload the window</p>
+  }
+}
+
+fetchAvailability = (manufacturer) =>{
+  // let config = {
+  //   headers: {
+  //     'Access-Control-Allow-Origin': "*"
+  //   }
+  // }
+  console.log(manufacturer)
+  // let returnAV
+  // let axiosPass = true;
+  //check if manufacturer is already fetched before
+  // this.manufacturerAV.every(manufac =>{
+  //   if(manufac.manufacturer === manufacturer){     
+  //     returnAV = manufac.data
+  //     axiosPass = false;
+  //     return false;
+  //   }
+  //   return true;
+  // })
   
-//   let returnAV
-//   let axiosPass = true;
-//   //check if manufacturer is already fetched before
-//   this.manufacturerAV.every(manufac =>{
-//     if(manufac.manufacturer === manufacturer){     
-//       returnAV = manufac.data
-//       axiosPass = false;
-//       return false;
-//     }
-//     return true;
-//   })
-  
-//   //console.log(manufacturer)
-//   if(axiosPass){
-//     await axios.get('/v2/availability/'+ manufacturer, config)
-//     .then(res => {
-//       if(res.status === 200){
-//         this.manufacturerAV.push({"manufacturer": manufacturer, "data": res.data.response})
-//         returnAV = res.data.response
-//       }
-//     }).catch(error =>{
-//       return console.log(error)
-//     })
-//   }
-//   return returnAV
-// }
+  // //console.log(manufacturer)
+  // if(axiosPass){
+  //   await axios.get('/v2/availability/'+ manufacturer, config)
+  //   .then(res => {
+  //     if(res.status === 200){
+  //       this.manufacturerAV.push({"manufacturer": manufacturer, "data": res.data.response})
+  //       returnAV = res.data.response
+  //     }
+  //   }).catch(error =>{
+  //     return console.log(error)
+  //   })
+  // }
+  // return returnAV
+}
 
 
 fetchCategories = () =>{
-  let categories = ["gloves", "beanies", "facemasks"]
+  let categories = ["gloves", "facemasks", "beanies"]
   let config = {
     headers: {
       'Access-Control-Allow-Origin': "*"
@@ -69,41 +83,58 @@ fetchCategories = () =>{
   categories.forEach(category => {  
     axios.get('/v2/products/' + category, config)
     .then(res=>{
+
+      let categoryTable = [];
+
       switch(category){
         case "beanies":
-          if(this.state.beanies == null){
-            this.setState({
-              beanies: res.data
-            })
-          }
-          
+          res.data.map((value, index) =>{
+            categoryTable.push(
+            <tr id={index}>
+              <td key="beanieId">{value.id}</td>
+              <td key="beanieName">{value.name}</td>
+              <td key="beanieColor">{value.color.join(", ")}</td>
+              <td key="beaniePrice">{value.price}</td>
+              <td key="beanieManufacturer"><a href="#" onClick={this.fetchAvailability}>{value.manufacturer}</a></td>
+            </tr>
+            )
+          })
+          this.setState({
+            currCategory: "beanies",
+            beanies: categoryTable
+          });
           return;
         case "gloves":
-          // let glovesCtgry = res.data;
-          
-          // glovesCtgry.forEach((gloves, index) =>{
-          //   let productAvblty = this.fetchAvailability(gloves.manufacturer)
-          //   console.log(productAvblty)
-          //   productAvblty.forEach(element => {
-          //     if(gloves.id === element.id){
-          //       glovesCtgry[index]["stockvalue"] = element.DATAPAYLOAD.match(this.stockRegex[1])
-          //     }
-          //   });
-            
-          // })
-          if(this.state.gloves == null){
-            this.setState({
-              gloves: res.data
-            })
-          }
+          res.data.map((value, index) =>{
+            categoryTable.push(
+            <tr id={index}>
+              <td key="gloveId">{value.id}</td>
+              <td key="gloveName">{value.name}</td>
+              <td key="gloveColor">{value.color.join(", ")}</td>
+              <td key="glovePrice">{value.price}</td>
+              <td key="gloveManufacturer"><a href="#" onClick={this.fetchAvailability}>{value.manufacturer}</a></td>
+            </tr>
+            )
+          })
+          this.setState({
+            gloves: categoryTable
+          });
           return;
         case "facemasks":
-          if(this.state.facemasks == null){
-            this.setState({
-              facemasks: res.data
-            })
-          }
-         
+          res.data.map((value, index) =>{
+            categoryTable.push(
+            <tr id={index}>
+              <td key="facemaskId">{value.id}</td>
+              <td key="facemaskName">{value.name}</td>
+              <td key="facemaskColor">{value.color.join(", ")}</td>
+              <td key="facemaskPrice">{value.price}</td>
+              <td key="facemaskManufacturer"><a href="#" onClick={this.fetchAvailability}>{value.manufacturer}</a></td>
+            </tr>
+            )
+          })
+          this.setState({
+            facemasks: categoryTable
+          });
           return;
         default:
           return
@@ -121,65 +152,62 @@ changeCategory = (category) => {
   this.setState({
     currCategory: category.target.id
   })
+  
 }
 
-getCategoryInfo = () =>{
-  let categoryTable = [];
-  switch(this.state.currCategory){
-    case "beanies":
-      this.state.beanies.map((value, index) =>{
-        categoryTable.push(
-        <tr id={index}>
-          <td key="id">{value.id}</td>
-          <td key="name">{value.name}</td>
-          <td key="color">{value.color.join(", ")}</td>
-          <td key="price">{value.price}</td>
-          <td key="manufacturer">{value.manufacturer}</td>
-        </tr>
-        )
-      })
-      return categoryTable;
-    case "gloves":
-      this.state.gloves.map((value, index) =>{
-        categoryTable.push(
-        <tr id={index}>
-          <td key="id">{value.id}</td>
-          <td key="name">{value.name}</td>
-          <td key="color">{value.color.join(", ")}</td>
-          <td key="price">{value.price}</td>
-          <td key="manufacturer">{value.manufacturer}</td>
-        </tr>
-        )
-      })
-      return categoryTable;
-    case "facemasks":
-      this.state.facemasks.map((value, index) =>{
-        categoryTable.push(
-        <tr id={index}>
-          <td key="id">{value.id}</td>
-          <td key="name">{value.name}</td>
-          <td key="color">{value.color.join(", ")}</td>
-          <td key="price">{value.price}</td>
-          <td key="manufacturer">{value.manufacturer}</td>
-        </tr>
-        )
-      })
-      return categoryTable;
-    default:
-      return categoryTable
-  }
+// getCategoryInfo = () =>{
+//   let categoryTable = [];
+//   switch(this.state.currCategory){
+//     case "beanies":
+//       this.state.beanies.map((value, index) =>{
+//         categoryTable.push(
+//         <tr id={index}>
+//           <td key="beanieId">{value.id}</td>
+//           <td key="beanieName">{value.name}</td>
+//           <td key="beanieColor">{value.color.join(", ")}</td>
+//           <td key="beaniePrice">{value.price}</td>
+//           <td key="beanieManufacturer"><a href="#" onClick={this.fetchAvailability}>{value.manufacturer}</a></td>
+//         </tr>
+//         )
+//       })
+//       return categoryTable;
+//     case "gloves":
+//       this.state.gloves.map((value, index) =>{
+//         categoryTable.push(
+//         <tr id={index}>
+//           <td key="gloveId">{value.id}</td>
+//           <td key="gloveName">{value.name}</td>
+//           <td key="gloveColor">{value.color.join(", ")}</td>
+//           <td key="glovePrice">{value.price}</td>
+//           <td key="gloveManufacturer"><a href="#" onClick={this.fetchAvailability}>{value.manufacturer}</a></td>
+//         </tr>
+//         )
+//       })
+//       return categoryTable;
+//     case "facemasks":
+//       this.state.facemasks.map((value, index) =>{
+//         categoryTable.push(
+//         <tr id={index}>
+//           <td key="facemaskId">{value.id}</td>
+//           <td key="facemaskName">{value.name}</td>
+//           <td key="facemaskColor">{value.color.join(", ")}</td>
+//           <td key="facemaskPrice">{value.price}</td>
+//           <td key="facemaskManufacturer"><a href="#" onClick={this.fetchAvailability}>{value.manufacturer}</a></td>
+//         </tr>
+//         )
+//       })
+//       return categoryTable;
+//     default:
+//       return categoryTable
+//   }
  
-}
+// }
 
 componentDidMount = () => {
   this.fetchCategories()
 }
 
   render(){
-    let category=<h6>Data is being fetched. Wait few seconds or press one of the category buttons on the top.</h6>;
-    if(this.state.gloves && this.state.beanies && this.state.facemasks){
-      category = this.getCategoryInfo()
-    }
     return(
       <Container>
         <div className="row">
@@ -192,12 +220,13 @@ componentDidMount = () => {
             <Table striped bordered hover>
                 <tr>
                   <th>ID</th>
-                  <th>{this.state.currCategory.charAt(0).toUpperCase() + this.state.currCategory.slice(1)} name</th>
+                  <th>Name of {this.state.currCategory}</th>
                   <th>Color(s)</th>
                   <th>Price €</th>
                   <th>Manufacturer</th>
                 </tr>
-                {category}
+                {this.state.currCategory == null ? <tr><th>Data Is being fetched. Wait a moment</th></tr> : this.getCategory()}
+                
             </Table>
           </div>
         </div>
